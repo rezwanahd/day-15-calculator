aebb8a64aced714f05c41535695a503cd6301a66
rezwanahd <hridoyk.13@gmail.com>
2022-02-02 02:08:49 +0600

    updated

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 
-const getvalue = [];
+let calData = [];
 
 let  getValue = (val) => {
 
-    
+    calData.push(val);
 
+
+    document.querySelector('.top-monitor').innerHTML = calData.join('');
+    document.querySelector('.main-monitor').innerHTML = 0;
+
+
+}
+
+let clear_all = () => {
+     
+    calData = [];
+
+    document.querySelector('.top-monitor').innerHTML = '';
+    document.querySelector('.main-monitor').innerHTML = '';
+}
+
+let finalRes = () => {
+
+    cal_string = calData.join('');
+
+    document.querySelector('.main-monitor').innerHTML = eval(cal_string)
 
 }
